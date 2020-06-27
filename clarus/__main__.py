0bc8e8c2c485e88b67dab0c2749637ab790bd5f8
Reinny <reynsth@gmail.com>
2020-06-27 14:54:22 -0400

    hot fix

--- clarus/__main__.py
+++ clarus/__main__.py
@@ -2,7 +2,6 @@ import argparse
 import sys
 from managers import database_manager as database
 from managers import database_translator as translator
-from managers import presentation as present
 
 # sets parser and adds arguments
 parser = argparse.ArgumentParser(description="Notes & Tasker App")
@@ -13,14 +12,21 @@ parser.add_argument("-c", "--content", type=str, dest="new_content", required="-
 
 args = parser.parse_args()
 
+def show_all():
+	for item in translator.full_log:
+		print(item)
+
 if __name__ == '__main__':
-	present.show_all()
+	show_all()
 
 	if argument := args.new_note:
 		database.add_data(argument)
+		show_all()
 
 	if argument := args.delete_note:
 		translator.delete_note(argument)
+		show_all()
 	
 	if argument := args.update_note:
 		translator.update_note(argument, args.new_content)
+		show_all()
--- clarus/managers/presentation.py
+++
@@ -1,5 +0,0 @@
-from .database_translator import full_log
-
-def show_all():
-	for item in full_log:
-		print(item)
