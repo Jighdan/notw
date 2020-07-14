fe13fd7635bf3acd7ffcc92bddf22f9ac838b66a
Reinny <reynsth@gmail.com>
2020-07-14 13:33:44 -0400

    :arrow_up: added required version

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 
 setup(
-	name = "notew",
-	author = "Reinny Almonte",
-	author_email = "reynsth@gmail.com",
-	url = "https://github.com/Jighdan/notew",
-	description = "A basic note taking CLI app.",
-	version = "0.1.0",
-	entry_points = {
-		"console_scripts": [
-			"notew = notew.__main__:main"
-		]
-	}
+    name = "notew",
+    author = "Reinny Almonte",
+    author_email = "reynsth@gmail.com",
+    url = "https://github.com/Jighdan/notew",
+    description = "A basic note taking CLI app.",
+    version = "0.1.0",
+    entry_points = {
+	"console_scripts": [
+	    "notew = notew.__main__:main"
+	]
+    },
+    python_requires=">=3.8"
 )
