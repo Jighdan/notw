aa3e51a4de0ebb8fb805bfcea0b3ca7f98ca0ca5
Reinny <reynsth@gmail.com>
2020-07-14 08:22:01 -0400

    :ambulance: hot fix

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+	name = "notew",
+	author = "Reinny Almonte",
+	author_email = "reynsth@gmail.com",
+	url = "https://github.com/Jighdan/notew",
+	description = "A basic note taking CLI app.",
+	version = "0.1.0",
+	entry_points = {
+		"console_scripts": [
+			"notew = notew.__main__:main"
+		]
+	}
+)
