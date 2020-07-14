8c9d0f1c6f8d8a9dd84c21a038900c2f952e17aa
Reinny <reynsth@gmail.com>
2020-07-14 11:18:08 -0400

    :fire: Improving argparse help system

--- notew/__main__.py
+++ notew/__main__.py
@@ -4,21 +4,21 @@ from . import database_translator as translator
 
 # sets parser and adds arguments
 parser = argparse.ArgumentParser(description="Notes & Tasker App")
-parser.add_argument("-n", "--new", type=str, dest="new_note", help="Adds a new note")
-parser.add_argument("-d", "--delete", type=int, dest="delete_note", help="Deletes a note")
-parser.add_argument("-u", "--update", type=int, dest="update_note", help="Updates a note content")
-parser.add_argument("-c", "--new_content", type=str, dest="new_content", required="--update" in sys.argv)
+parser.add_argument("-n", "--new", type=str, help="Adds a new note")
+parser.add_argument("-d", "--delete", type=int, help="Deletes a note")
+parser.add_argument("-u", "--update", type=int, help="Updates a note content")
+parser.add_argument("-c", "--new_content", type=str, required="--update" in sys.argv)
 
 args = parser.parse_args()
 
 def main():
-	if argument := args.new_note:
+	if argument := args.new:
 		translator.add_note(argument)
 
-	if argument := args.delete_note:
+	if argument := args.delete:
 		translator.delete_note(argument)
 	
-	if argument := args.update_note:
+	if argument := args.update:
 		translator.update_note(argument, args.new_content)
 
 	for item in translator.presentable_data():
