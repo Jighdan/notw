e500357732bd954e1f5ed1249db27e6088b46f20
Reinny <reynsth@gmail.com>
2020-07-22 16:34:50 -0400

    :pencil: Project rename

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name = "notew",
+    name = "notw",
     author = "Reinny Almonte",
     author_email = "reynsth@gmail.com",
-    url = "https://github.com/Jighdan/notew",
+    url = "https://github.com/Jighdan/notw",
     description = "A basic note taking CLI app.",
     version = "0.1.0",
     entry_points = {
 	"console_scripts": [
-	    "notew = notew.__main__:main"
+	    "notw = notw.__main__:main"
 	]
     },
     python_requires=">=3.8"
